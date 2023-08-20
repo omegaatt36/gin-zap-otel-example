297256e252b2599510f5850757c75ae3431d9724
Raiven Kao <omegaatt36@gmail.com>
2023-08-20 12:56:33 +0800

    wrap log functions.

--- logging/logger.go
+++ logging/logger.go
@@ -92,8 +92,8 @@ func Init(cfg Config) {
 	Warnf = logger.Sugar().Warnf
 }
 
-type printWrapper func(args ...interface{})
-type printfWrapper func(template string, args ...interface{})
+type printWrapper func(args ...any)
+type printfWrapper func(template string, args ...any)
 
 var (
 	Info   printWrapper
@@ -109,3 +109,123 @@ var (
 	Warn   printWrapper
 	Warnf  printfWrapper
 )
+
+// DebugCtx logs a message at level Debug on the logger associated with the context.
+func DebugCtx(ctx context.Context, message any) {
+	FromContext(ctx).Sugar().Debug(message)
+}
+
+// DebugfCtx logs a message at level Debug on the logger associated with the context.
+func DebugfCtx(ctx context.Context, template string, args ...any) {
+	FromContext(ctx).Sugar().Debugf(template, args...)
+}
+
+// DebugWithData logs a message at level Debug on the logger associated with the context.
+func DebugWithData(message any, data any) {
+	logger.With(zap.Any("data", data)).Sugar().Debug(message)
+}
+
+// DebugWithDataCtx logs a message at level Debug on the logger associated with the context.
+func DebugWithDataCtx(ctx context.Context, message any, data any) {
+	FromContext(ctx).With(zap.Any("data", data)).Sugar().Debug(message)
+}
+
+// InfoCtx logs a message at level Info on the logger associated with the context.
+func InfoCtx(ctx context.Context, message any) {
+	FromContext(ctx).Sugar().Info(message)
+}
+
+// InfofCtx logs a message at level Info on the logger associated with the context.
+func InfofCtx(ctx context.Context, template string, args ...any) {
+	FromContext(ctx).Sugar().Infof(template, args...)
+}
+
+// InfoWithData logs a message at level Info on the logger associated with the context.
+func InfoWithData(message any, data any) {
+	logger.With(zap.Any("data", data)).Sugar().Info(message)
+}
+
+// InfoWithDataCtx logs a message at level Info on the logger associated with the context.
+func InfoWithDataCtx(ctx context.Context, message any, data any) {
+	FromContext(ctx).With(zap.Any("data", data)).Sugar().Info(message)
+}
+
+// WarnCtx logs a message at level Warn on the logger associated with the context.
+func WarnCtx(ctx context.Context, message any) {
+	FromContext(ctx).Sugar().Warn(message)
+}
+
+// WarnfCtx logs a message at level Warn on the logger associated with the context.
+func WarnfCtx(ctx context.Context, template string, args ...any) {
+	FromContext(ctx).Sugar().Warnf(template, args...)
+}
+
+// WarnWithData logs a message at level Warn on the logger associated with the context.
+func WarnWithData(message any, data any) {
+	logger.With(zap.Any("data", data)).Sugar().Warn(message)
+}
+
+// WarnWithDataCtx logs a message at level Warn on the logger associated with the context.
+func WarnWithDataCtx(ctx context.Context, message any, data any) {
+	FromContext(ctx).With(zap.Any("data", data)).Sugar().Warn(message)
+}
+
+// ErrorCtx logs a message at level Error on the logger associated with the context.
+func ErrorCtx(ctx context.Context, message any) {
+	FromContext(ctx).Sugar().Error(message)
+}
+
+// ErrorfCtx logs a message at level Error on the logger associated with the context.
+func ErrorfCtx(ctx context.Context, template string, args ...any) {
+	FromContext(ctx).Sugar().Errorf(template, args...)
+}
+
+// ErrorWithData logs a message at level Error on the logger associated with the context.
+func ErrorWithData(message any, data any) {
+	logger.With(zap.Any("data", data)).Sugar().Error(message)
+}
+
+// ErrorWithDataCtx logs a message at level Error on the logger associated with the context.
+func ErrorWithDataCtx(ctx context.Context, message any, data any) {
+	FromContext(ctx).With(zap.Any("data", data)).Sugar().Error(message)
+}
+
+// FatalCtx logs a message at level Fatal on the logger associated with the context.
+func FatalCtx(ctx context.Context, message any) {
+	FromContext(ctx).Sugar().Fatal(message)
+}
+
+// FatalfCtx logs a message at level Fatal on the logger associated with the context.
+func FatalfCtx(ctx context.Context, template string, args ...any) {
+	FromContext(ctx).Sugar().Fatalf(template, args...)
+}
+
+// FatalWithData logs a message at level Fatal on the logger associated with the context.
+func FatalWithData(message any, data any) {
+	logger.With(zap.Any("data", data)).Sugar().Fatal(message)
+}
+
+// FatalWithDataCtx logs a message at level Fatal on the logger associated with the context.
+func FatalWithDataCtx(ctx context.Context, message any, data any) {
+	FromContext(ctx).With(zap.Any("data", data)).Sugar().Fatal(message)
+}
+
+// PanicCtx logs a message at level Panic on the logger associated with the context.
+func PanicCtx(ctx context.Context, message any) {
+	FromContext(ctx).Sugar().Panic(message)
+}
+
+// PanicfCtx logs a message at level Panic on the logger associated with the context.
+func PanicfCtx(ctx context.Context, template string, args ...any) {
+	FromContext(ctx).Sugar().Panicf(template, args...)
+}
+
+// PanicWithData logs a message at level Panic on the logger associated with the context.
+func PanicWithData(message any, data any) {
+	logger.With(zap.Any("data", data)).Sugar().Panic(message)
+}
+
+// PanicWithDataCtx logs a message at level Panic on the logger associated with the context.
+func PanicWithDataCtx(ctx context.Context, message any, data any) {
+	FromContext(ctx).With(zap.Any("data", data)).Sugar().Panic(message)
+}
